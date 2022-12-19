9960e6df92bbd2f003bf0efac8eb88c05bfa1a53
Smirnow Misha <113326729+BallBoychick@users.noreply.github.com>
2022-12-19 10:54:49 +0600

    Develop (#9)
    * fcom
    
    * scom
    
    * thcom
    
    * forfun
    
    * actions
    
    * forskip
    
    * general
    
    * edit_vector
    
    * edit_csproj_ms_build
    
    * edit_csproj_ms_build
    
    * ms_build
    
    * edit_execute_movecommand
    
    * first_wrong_test
    
    * 2_right_tests
    
    * move_command_tests_done
    
    * move_command_tests_done
    
    * move_command_tests_done
    
    * is_it_action?
    
    * ups
    
    * is_it_action?
    
    * all_about_Rotate_besides_class_Angle
    
    * love_codecov
    
    * codecove_100%
    
    * codecove_100%
    
    * edit_structure
    
    * edit_structure
    
    * testing_vector
    
    * testing_vector
    
    * testing_vector
    
    * testing_vector
    
    * testing_vector
    
    * testing_vector
    
    * testing_vector
    
    * testing_vector
    
    * testing_vector
    
    * testing_vector
    
    * testing_vector
    
    * again_cov_100%
    
    * Angle_tests
    
    * Angle_tests
    
    * Angle_tests
    
    * Angle_tests
    
    * Angle_tests_97%
    
    * Angle_tests_97%
    
    * Angle_tests_100%
    
    * Angle_tests_97%
    
    * Angle_tests_97%
    
    * Angle_tests_97%
    
    * Angle_tests_100%
    
    * Angle_tests_100%
    
    * Angle_tests_100%
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * last_change
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * last_change
    
    * last_change
    
    * last_change
    
    * last_change
    
    * yml_edit
    
    * yml
    
    * yml
    
    * yml
    
    * yml
    
    * yml
    
    * yml?
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * Update README.md
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * yml_edit
    
    * Update README.md
    
    * yml_edit
    
    * yml_edit
    
    * last_yml_edit
    
    * last_edit_angle
    
    * last_edit_angle
    
    * last,...,commit
    
    * fix_angle
    
    Co-authored-by: Mikhail <smirnow.mishas@gmail.com>

---
+++ SpaceBattle.Lib.Test/AngleTest.cs
@@ -0,0 +1,56 @@
+namespace SpaceBattle.Lib.Test;
+public class AngleTest
+{
+    [Fact]
+    public void AngleTestSum()
+    {
+        Angle a = new Angle(1, 9);
+        Angle b = new Angle(2, 3);
+        Angle c = new Angle(5, 9);
+        Assert.Equal(new Angle(7, 9), a + b);
+        Assert.Equal(new Angle(11, 9), c + b);
+        Assert.Equal(new Angle(11, 9), b + c);
+    }
+    [Fact]
+    public void AngleTestEqual()
+    {
+        Assert.Throws<DivideByZeroException>(() => new Angle(1, 0));
+    }
+    [Fact]
+    public void AngleEqualEqualTest()
+    {
+        Angle a = new Angle(1, 3);
+        Angle b = new Angle(2, 3);
+        Assert.True(a != b);
+    }
+    [Fact]
+    public void AngleNotEqualTest()
+    {
+        Angle a = new Angle(1, 3);
+        Angle b = new Angle(1, 3);
+        Assert.True(a == b);
+
+    }
+    [Fact]
+    public void VectorPositiveNotEqualsTest()
+    {
+        Angle a = new Angle(45, 1);
+        Assert.Equal("45/1", a.ToString());
+    }
+    [Fact]
+    public void GetHashCodeTest()
+    {
+        Angle a = new Angle(1, 2);
+        Angle b = new Angle(1, 2);
+        Assert.Equal(a.GetHashCode(), b.GetHashCode());
+    }
+    [Fact]
+    public void EqualsNegativeTest()
+    {
+        Angle a = new Angle(1, 2);
+        Angle c = new Angle(2, 1);
+        int b = 3;
+        Assert.False(a.Equals(b));
+        Assert.False(a.Equals(c));
+    }
+};
---
+++ SpaceBattle.Lib/Angle/Angle.cs
@@ -0,0 +1,46 @@
+namespace SpaceBattle.Lib;
+public class Angle
+{
+    public int numa;
+    public int dena;
+    public Angle(int angle1, int angle2)
+    {
+        numa = angle1;
+        dena = angle2;
+        if (dena == 0)
+        {
+            throw new System.DivideByZeroException();
+        }
+    }
+    public static int GCD(int x, int y)
+    {
+    return y == 0 ? x : GCD(y, x % y);
+    }
+    public override string ToString()
+    {
+        return numa.ToString() + "/" + dena.ToString();
+    }
+    public static Angle operator +(Angle angle1, Angle angle2)
+    {
+        int y3 = GCD(angle1.numa * angle2.dena + angle2.numa * angle1.dena, angle1.dena * angle2.dena);
+        return new Angle((angle1.numa * angle2.dena + angle2.numa * angle1.dena) / y3, angle1.dena * angle2.dena / y3);
+        
+    }
+    public static bool operator ==(Angle angle1, Angle angle2)
+    {
+        return (decimal)angle1.numa * angle2.dena == (decimal)angle2.numa * angle1.dena;
+    }
+    public static bool operator !=(Angle angle1, Angle angle2)
+    {
+        return (!(angle1 == angle2));
+    }
+    public override bool Equals(object? obj)
+    {
+        return obj is Angle angle &&
+         numa * angle.dena == angle.numa * dena;
+    }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(numa, dena);
+    }
+}
