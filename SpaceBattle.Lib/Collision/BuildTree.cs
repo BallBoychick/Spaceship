8ad805ce540400af6ab7572c453b3778cad7788e
Smirnow Misha <113326729+BallBoychick@users.noreply.github.com>
2022-12-27 07:59:52 +0000

    Online

--- SpaceBattle.Lib/Collision/BuildTree.cs
+++ SpaceBattle.Lib/Collision/BuildTree.cs
@@ -1,3 +1,35 @@
+// using System.IO;
+// using System.Collections.Generic;
+// using Hwdtech;
+// namespace SpaceBattle.Lib;
+
+
+// public class CSVReader: ICommand
+// {
+//     private string spliter;
+//     private string fileway;
+//     public CSVReader(string fileway, string spliter="; ")
+//     {
+//         this.fileway = fileway;
+//         this.spliter = spliter;
+//     }
+
+//     public void execute()
+//     {
+//         using (var reader = new StreamReader(fileway))
+//         {
+//             IList<string> listHEADERS = reader.ReadLine().Split("; ");
+//             List<Dictionary<string, double>> table = new List<Dictionary<string, double>>();
+            
+//             while (!reader.EndOfStream)
+//             {
+//                 var values = reader.ReadLine().Split("; ");
+//                 table.Add(new Dictionary<string, double>(listHEADERS.Zip(values, (k, v) => new KeyValuePair<string, double>(k, Convert.ToDouble(v)))));
+//             }
+//         }
+//     }
+// }
+
 // using System;
 //    using Microsoft.ML;
 //    using Microsoft.ML.Data;
