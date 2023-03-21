a14fbb974bbefd1aa85d17488812651d40f20e4d
DVers13 <91010704+DVers13@users.noreply.github.com>
2023-03-21 13:26:28 +0600

    create_server_thread

---
+++ SpaceBattle.Lib/ServerThread/ServerThread.cs
@@ -0,0 +1,61 @@
+namespace SpaceBattle.Lib;
+using Hwdtech;
+public class ServerThread
+{
+    private bool stop = false;
+    internal IReciever queue;
+
+    private Action strategy;
+    internal Thread thread;
+    public ServerThread(IReciever reciever)
+    {
+        queue = reciever;
+
+        strategy = () =>
+        {
+            HandleCommand();
+        };
+        thread = new Thread(() =>
+        {
+            IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+            while (!stop)
+            {
+                try
+                {
+                    strategy();
+                }
+                catch (Exception e)
+                {
+                    IoC.Resolve<ICommand>("Exception Handler.Add", e).execute();
+
+
+                }
+
+            }
+        }
+         );
+
+    }
+
+    public void StartThread()
+    {
+        thread.Start();
+    }
+
+    public void StopThread()
+    {
+        stop = true;
+
+    }
+
+    internal void ChangeBehaviour(Action changeBehaviour)
+    {
+        strategy = changeBehaviour;
+    }
+
+    internal void HandleCommand()
+    {
+        ICommand command = queue.Recieve();
+        command.execute();
+    }
+}
