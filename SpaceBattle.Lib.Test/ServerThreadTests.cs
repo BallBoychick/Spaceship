305b2084b17b3265e74b63166a945d60cf3fc224
DVers13 <91010704+DVers13@users.noreply.github.com>
2023-03-21 17:34:57 +0600

    create_test_1

---
+++ SpaceBattle.Lib.Test/ServerThreadTests.cs
@@ -0,0 +1,49 @@
+namespace SpaceBattle.Lib.Test;
+using Moq;
+using System;
+using Xunit;
+using Hwdtech.Ioc;
+using Hwdtech;
+using SpaceBattle.Lib;
+
+public class ServerThreadTests
+{
+    public ServerThreadTests()
+    {
+
+    }
+    private void InitState()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+        
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Create And Start Thread", (object[] param) => new CreateStartServerThreadStrategy().RunStrategy(param)).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Send Command", (object[] param) =>
+        {
+            var sendCommand = new Mock<Lib.ICommand>();
+            sendCommand.Setup(x => x.execute()).Callback(() => new SendCommandStrategy().RunStrategy(param)); return sendCommand.Object;
+        }).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Hard Stop Command", (object[] param) =>
+        {
+            var hardStopThreadCommand = new Mock<Lib.ICommand>();
+            hardStopThreadCommand.Setup(x => x.execute()).Callback(() => new HardStopThreadStrategy().RunStrategy(param)); return hardStopThreadCommand.Object;
+        }).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Soft Stop Command", (object[] param) =>
+        {
+            var softStopThreadCommand = new Mock<Lib.ICommand>();
+            softStopThreadCommand.Setup(x => x.execute()).Callback(() => new SoftStopThreadStrategy().RunStrategy(param)); return softStopThreadCommand.Object;
+        }).Execute();
+
+
+
+
+
+    }
+
+    [Fact]
+    public void StartThreadSuccess()
+    {
+        InitState();
+
+    }
+}
