844bcb19208ed1dfb336d2c4d22616c2c4d56808
Mikhail <smirnow.mishas2017@gmail.com>
2023-03-26 14:51:23 +0600

    main_structure_and_realize_StartApp

---
+++ SpaceBattle.Lib/Console_Tread/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SpaceBattle.Lib;
+using Hwdtech;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //вызываем из StartApp и execute()
+    }
+}
---
+++ SpaceBattle.Lib/Console_Tread/StartApp.cs
@@ -0,0 +1,20 @@
+namespace SpaceBattle.Lib;
+using Hwdtech;
+
+class StartApp
+{
+    //public что-то
+    public int numberthread;
+
+    public StartApp(int numberthread)
+    {
+        this.numberthread = numberthread;
+    }
+    public void Execute()
+    {
+        //writeline
+        //нажмите для запуска потока
+        //нажмите для остановки потока
+        //мб можно стратегию замутить
+    }
+}
