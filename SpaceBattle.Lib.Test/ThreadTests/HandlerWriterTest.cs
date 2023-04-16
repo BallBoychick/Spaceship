e5dcc4f98ebd356539c450f19065ad00857c883c
Mikhail <smirnow.mishas2017@gmail.com>
2023-04-16 23:50:56 +0600

    handlerwriter

---
+++ SpaceBattle.Lib.Test/ThreadTests/HandlerWriterTest.cs
@@ -0,0 +1,25 @@
+using Hwdtech;
+using Hwdtech.Ioc;
+namespace SpaceBattle.Lib.Test;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+public class HandlerWriterTests
+{
+
+    [Fact]
+    public void Correct_Writer_ExecuteAsync()
+    {
+        var path = "../../../Errors.txt";
+        var cmd = new HandlerWriterCommand(path, "Error3");
+        cmd.Execute();
+        var sr = new StreamReader(path);
+
+        var str = File.ReadLines(path).ToList().Last();
+
+        Assert.Equal("Error3", str);
+    }
+}
