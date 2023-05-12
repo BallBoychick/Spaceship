ccb489fc7c7aa639c8ecf0520dbf5d6c84fbf40d
Mikhail <smirnow.mishas2017@gmail.com>
2023-05-12 23:30:21 +0600

    interpret_and_Create_command_start_tests

---
+++ SpaceBattle.Lib/Message_processing/CreateCommandStrategy.cs
@@ -0,0 +1,17 @@
+using Hwdtech;
+using System;
+namespace SpaceBattle.Lib;
+
+public class CreateCommandStrategy : IStrategy
+{
+    public object RunStrategy(params object[] args)
+    {
+        var message = (IMessage)args[0];
+
+        var OrderType = message.OrderType;
+
+        var gameitemid = message.GameItemID;
+
+        var properties = message.Properties;
+    }
+}
