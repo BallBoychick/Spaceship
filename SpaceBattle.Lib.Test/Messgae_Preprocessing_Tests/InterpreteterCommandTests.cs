3605facfc541879652b15e96fcee67f6361fd73f
Mikhail <smirnow.mishas2017@gmail.com>
2023-05-15 22:48:51 +0600

    game_set_properties_tests

--- SpaceBattle.Lib.Test/Messgae_Preprocessing_Tests/InterpreteterCommandTests.cs
+++ SpaceBattle.Lib.Test/Messgae_Preprocessing_Tests/InterpreteterCommandTests.cs
@@ -6,50 +6,77 @@ namespace SpaceBattle.Lib.Test;
 public class InterpreterCommandTests
 {
     [Fact]
-        public void TestExecute()
-        {
-            new InitScopeBasedIoCImplementationCommand().Execute();
-            IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+    public void TestExecute()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
 
-            var mockCommand = new Mock<ICommand>();
-            mockCommand.Setup(x => x.Execute());
+        var mockCommand = new Mock<ICommand>();
+        mockCommand.Setup(x => x.Execute());
 
-            var message = new Mock<IMessage>();
-            message.Setup(m => m.GameID).Throws<Exception>();
+        var message = new Mock<IMessage>();
+        message.Setup(m => m.GameID).Throws<Exception>();
 
-            var mockStrategyReturnsCommand = new Mock<IStrategy>();
-            mockStrategyReturnsCommand.Setup(x => x.RunStrategy(It.IsAny<IMessage>())).Returns(mockCommand.Object);
+        var mockStrategyReturnsCommand = new Mock<IStrategy>();
+        mockStrategyReturnsCommand.Setup(x => x.RunStrategy(It.IsAny<IMessage>())).Returns(mockCommand.Object);
 
-            var queue = new Queue<ICommand>();
+        var queue = new Queue<ICommand>();
 
-            // var mock_strategy = new Mock<IStrategy>();
-            // mock_strategy.Setup(c => c.RunStrategy(It.IsAny<object[]>())).Returns(queue).Verifiable();
-
-
-            var queuepushCommand = new Mock<ICommand>();
-            queuepushCommand.Setup(c => c.Execute()).Callback(() => { queue.Enqueue(mockCommand.Object); });
-
-            var queuePushStrategy = new Mock<IStrategy>();
-            queuePushStrategy.Setup(s => s.RunStrategy(It.IsAny<int>(), It.IsAny<ICommand>())).Returns(queuepushCommand.Object).Verifiable();
-
-            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.CreateCommand", (object[] args) => mockStrategyReturnsCommand.Object.RunStrategy(args)).Execute();
-            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Queue.Push", (object[] args) => queuePushStrategy.Object.RunStrategy(args)).Execute();
-
-            var interpreterCommand = new InterpreterCommand(message.Object);
-
-            Assert.Throws<Exception>(() => interpreterCommand.Execute());
-            // queue.Verify(q => q.Push(It.IsAny<string>(), gameCommandMock.Object)); 
+        // var mock_strategy = new Mock<IStrategy>();
+        // mock_strategy.Setup(c => c.RunStrategy(It.IsAny<object[]>())).Returns(queue).Verifiable();
 
 
-        }
+        var queuepushCommand = new Mock<ICommand>();
+        queuepushCommand.Setup(c => c.Execute()).Callback(() => { queue.Enqueue(mockCommand.Object); });
+
+        var queuePushStrategy = new Mock<IStrategy>();
+        queuePushStrategy.Setup(s => s.RunStrategy(It.IsAny<int>(), It.IsAny<ICommand>())).Returns(queuepushCommand.Object).Verifiable();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.CreateCommand", (object[] args) => mockStrategyReturnsCommand.Object.RunStrategy(args)).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Queue.Push", (object[] args) => queuePushStrategy.Object.RunStrategy(args)).Execute();
+
+        var interpreterCommand = new InterpreterCommand(message.Object);
+
+        Assert.Throws<Exception>(() => interpreterCommand.Execute());
+        // queue.Verify(q => q.Push(It.IsAny<string>(), gameCommandMock.Object));
+            
+
+
+    }
+    
     [Fact]
     public void InterpreterCommand_CanBeConstructed()
     {
-        var commandMock = new Mock<ICommand>();
-        var message = Mock.Of<IMessage>();
-        var InterpreterCommand = new InterpreterCommand(message);
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+        var message = new Mock<IMessage>();
+        message.Setup(m => m.GameID).Returns(1);
 
-        Assert.NotNull(InterpreterCommand);
+
+        var commandMock = new Mock<ICommand>();
+        commandMock.Setup(x => x.Execute());
+        // var mockStrategyReturnsCommand = new Mock<IStrategy>();
+        // mockStrategyReturnsCommand.Setup(x => x.RunStrategy(It.IsAny<IMessage>())).Returns(mockCommand.Object);
+        var queue = new Queue<ICommand>();
+
+        var queuepushCommand = new Mock<ICommand>();
+        queuepushCommand.Setup(c => c.Execute()).Callback(() => { queue.Enqueue(commandMock.Object); });
+
+        var queuePushStrategy = new Mock<IStrategy>();
+        queuePushStrategy.Setup(s => s.RunStrategy(It.IsAny<int>(), It.IsAny<ICommand>())).Returns(queuepushCommand.Object).Verifiable();
+
+        var mockStrategyReturnsCommand = new Mock<IStrategy>();
+        mockStrategyReturnsCommand.Setup(x => x.RunStrategy(It.IsAny<IMessage>())).Returns(commandMock.Object);
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.CreateCommand", (object[] args) => mockStrategyReturnsCommand.Object.RunStrategy(args)).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Queue.Push", (object[] args) => queuePushStrategy.Object.RunStrategy(args)).Execute();
+
+        var InterpreterCommand = new InterpreterCommand(message.Object);
+
+        InterpreterCommand.Execute();
+
+        queuePushStrategy.VerifyAll();
+
     }
         
 }
