56fb0759c0a83dbee305b6db2408d94354c4b484
DVers13 <91010704+DVers13@users.noreply.github.com>
2023-05-15 15:54:14 +0600

    edit_3

--- SpaceBattle.Lib.Test/SendMessageStrategyTests.cs
+++ SpaceBattle.Lib.Test/SendMessageStrategyTests.cs
@@ -6,15 +6,18 @@ using Hwdtech.Ioc;
 using Hwdtech;
 using SpaceBattle.Lib;
 
-public class SendMessageStrategyTests{
-    public SendMessageStrategyTests(){
+public class SendMessageStrategyTests
+{
+    public SendMessageStrategyTests()
+    {
         new InitScopeBasedIoCImplementationCommand().Execute();
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Send Message", (object [] parameters) => new SendMessageStrategy().RunStrategy(parameters)).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Send Message", (object[] parameters) => new SendMessageStrategy().RunStrategy(parameters)).Execute();
 
     }
     [Fact]
-    public void MessagePushInQueueSuccess(){
+    public void MessagePushInQueueSuccess()
+    {
         var queueMessageThread = new BlockingCollection<IMessage>();
         var mockSender = new Mock<ISender>();
         var command = new Mock<SpaceBattle.Lib.ICommand>();
@@ -22,8 +25,8 @@ public class SendMessageStrategyTests{
         var threadID = "1";
         messageFromJson.SetupGet(message => message.GameID).Returns(threadID);
         mockSender.Setup(sender => sender.Send(It.IsAny<SpaceBattle.Lib.ICommand>())).Callback(() => queueMessageThread.Add(messageFromJson.Object)).Verifiable();
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Thread.QueueMessages" + threadID, (object [] parameters) => mockSender.Object).Execute();
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "CommandFromMessageStrategy", (object [] parameters) => command.Object).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Thread.QueueMessages" + threadID, (object[] parameters) => mockSender.Object).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "CommandFromMessageStrategy", (object[] parameters) => command.Object).Execute();
         IoC.Resolve<bool>("Send Message", messageFromJson.Object);
         mockSender.Verify();
         Assert.True(queueMessageThread.Take().GameID == "1");
